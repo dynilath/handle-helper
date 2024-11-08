1f97b6cc7e550ebaa1ba20a1c03bd0285f10c268
dynilath <dynilath@qq.com>
2024-11-08 22:00:11 +0800

    更好看的的输出

--- handle.py
+++ handle.py
@@ -29,15 +29,17 @@ if __name__ == "__main__":
         word_to_pinyin_map.setdefault(w[0][0], w[0][1])
 
     print("这是一个汉兜（https://handle.antfu.me/）求解工具")
+    print("")
     print("输入范例：")
     print("不可一世 0000 1000 0010 0112")
-
     print("第一组数字0000，表示“不可一世”四个字没有一个正确")
     print("第二组数字1000，表示“不可一世”四个字的拼音中，不bù的辅音b在答案中存在")
     print("第三组数字0010，表示“不可一世”四个字的拼音中，一yī的元音i在答案中存在")
     print("第四组数字0112，表示“不可一世”四个字的拼音中，可kě的音调、一yī的音调、世shì的音调在答案中存在，且世shì的音调位置也正确")
 
     while True:
+        print("")
+        print("推荐词(信息熵排序)：")
         print_first5(possible_with_weight)
 
         if len(possible_with_weight) < 2:
